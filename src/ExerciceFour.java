bc81eed11b7fe462fa3cbf441b0e0b4ecaae8a15
Melissa Kouadio <angemelisk@gmail.com>
2023-02-06 05:13:24 +0000

    week6 day2 xp exercice

---
+++ src/ExerciceFour.java
@@ -0,0 +1,14 @@
+public class ExerciceFour {
+
+        public static void main(String[] args) {
+            printSum();
+        }
+
+        public static void printSum() {
+            byte numOne = 104;
+            byte numTwo = 110;
+            int sumOfTwoNumbers;
+            sumOfTwoNumbers = (int)(numOne + numTwo);
+            System.out.print("Sum: " + sumOfTwoNumbers);
+        }
+}
